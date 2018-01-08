db354fbdbbf29f92eed86df04b8773eef2958170
GojkoMaric <gojko.maric@yahoo.com>
2018-01-08 21:02:25 +0100

    Dodavanje komponente u app komponentu (index.html)

--- src/app/contacts-list/contacts-list.component.ts
+++ src/app/contacts-list/contacts-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-contacts-list',
+  selector: 'app-contacts-list', //selector se uzima i stavlja u app.component pod tag <app-...>
   templateUrl: './contacts-list.component.html',
   styleUrls: ['./contacts-list.component.css']
 })
