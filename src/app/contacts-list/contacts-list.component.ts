4ac7d8136b089c00dd6a126b47e1e64fff3bbb0d
GojkoMaric <gojko.maric@yahoo.com>
2018-01-09 18:56:42 +0100

    pravljenje inputa za tekst i  izmene u modulu

--- src/app/contacts-list/contacts-list.component.ts
+++ src/app/contacts-list/contacts-list.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ContactsListComponent implements OnInit {
 
   contacts;
+  testModel = "test";
 
   constructor() { 
     this.contacts=
