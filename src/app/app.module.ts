dca8e02868474a372dd01e6321f88f6d6f95a0a9
GojkoMaric <gojko.maric@yahoo.com>
2018-01-09 18:38:09 +0100

    ubacivanje nav-bar komponente i dodavanje bootstrap forme za kontakte

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ContactsListComponent } from './contacts-list/contacts-list.component';
 import { LayoutComponent } from './layout/layout.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactsListComponent,
-    LayoutComponent
+    LayoutComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule
